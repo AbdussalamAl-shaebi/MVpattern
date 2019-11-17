5efdc8090a18d3e1060c62929f54ef0e775fb9f2
AbdussalamAl-shaebi <ashuaebi.abdussalam@gmail.com>
2019-11-17 21:18:07 +0300

    add project and database

---
+++ C# code/MVVMPattern/MVVMPattern/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MVVMPattern.View;
+using MVVMPattern.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MVVMPattern
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainPage window = new MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
---
+++ C# code/MVVMPattern/MVVMPattern/Model/User .cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace MVVMPattern.Model
+{
+    class User:INotifyPropertyChanged
+    {
+        private int userId;
+        private string fristName;
+        private string lastName;
+        private string city;
+        private string state;
+        private string country;
+
+        
+
+        public int UserId
+        {
+            get => userId;
+            set {
+                userId = value;
+                OnPropertyChanged("UserId");
+            }
+        }
+
+        public string FirstName
+        {
+            get => fristName;
+            set
+            {
+                fristName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                OnPropertyChanged("City");
+
+            }
+        }
+
+
+        public string State
+        {
+            get => state;
+            set
+            {
+                state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+        public string Country
+        {
+            get => country;
+            set
+            {
+                country = value;
+                OnPropertyChanged("Country");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string proprtyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proprtyName));
+        }
+    }
+
+
+}
